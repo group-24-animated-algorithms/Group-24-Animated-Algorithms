9f15c742a0971fa26b5bda7b2b528d1d285c764b
Callum Dyson-Gainsborough <cgainsborough@hotmail.com>
2019-03-17 13:41:32 +0000

    Added Quicksort
    fixed issues we were having when we first added quick sort

--- Group 24 Animated Algorithms/Sorting Algorithms/Quick.cs
+++ Group 24 Animated Algorithms/Sorting Algorithms/Quick.cs
@@ -23,12 +23,128 @@ namespace Group_24_Animated_Algorithms.Sorting_Algorithms
 
         public void Ascending(Decimal[] input)
         {
+            //creates counter
+            int count = 0;
 
+            //call initial sort
+            SortAscending(input, 0, input.Length - 1, ref count);
+        }
+
+        private void SortAscending(decimal[] input, int start, int end, ref int count)
+        {
+            //get current and split arrays in reccursion
+            int current = PartitionAscending(input, start, end, ref count);
+
+            //left array
+            if (start < current - 1)
+            {
+                SortAscending(input, start, current - 1, ref count);
+            }
+
+            //right array
+            if (end > current)
+            {
+                SortAscending(input, current, end, ref count);
+            }
+        }
+
+        private int PartitionAscending(decimal[] array, int left, int right, ref int count)
+        {
+            //add counter
+            count++;
+
+            //get pivot
+            decimal pivot = array[left];
+
+            while (left <= right)
+            {
+                //getting numbers less than pivot
+                while (array[left] < pivot)
+                {
+                    left++;
+                }
+                //getting numbers greater than pivot
+                while (array[right] > pivot)
+                {
+                    right--;
+                }
+
+                //swap
+                if (left <= right)
+                {
+                    Output.SwapBars(left, array[left], right, array[right]);
+                    ////Update("swap", "code here");
+                    decimal tmp = array[left];
+                    array[left] = array[right];
+                    array[right] = tmp;
+
+                    left++;
+                    right--;
+                }
+            }
+            return left;
         }
 
         public void Descending(Decimal[] input)
         {
+            //creates counter
+            int count = 0;
 
+            //call initial sort
+            SortDescending(input, 0, input.Length - 1, ref count);
+        }
+
+        private void SortDescending(decimal[] input, int start, int end, ref int count)
+        {
+            //get current and split arrays in reccursion
+            int current = PartitionDescending(input, start, end, ref count);
+
+            //left array
+            if (start < current - 1)
+            {
+                SortDescending(input, start, current - 1, ref count);
+            }
+
+            //right array
+            if (end > current)
+            {
+                SortDescending(input, current, end, ref count);
+            }
+        }
+
+        private int PartitionDescending(decimal[] array, int left, int right, ref int count)
+        {
+            //add counter
+            count++;
+
+            //get pivot
+            decimal pivot = array[left];
+
+            while (left <= right)
+            {
+                //getting numbers less than pivot
+                while (array[left] > pivot)
+                {
+                    left++;
+                }
+                //getting numbers greater than pivot
+                while (array[right] < pivot)
+                {
+                    right--;
+                }
+
+                //swap
+                if (left <= right)
+                {
+                    decimal tmp = array[left];
+                    array[left] = array[right];
+                    array[right] = tmp;
+
+                    left++;
+                    right--;
+                }
+            }
+            return left;
         }
     }
 }
